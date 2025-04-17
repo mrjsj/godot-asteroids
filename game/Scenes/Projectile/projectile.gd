b3673a66ca8823b5fdb0ee8d825d160e454af97b
mrjsj <22403319+mrjsj@users.noreply.github.com>
2025-04-17 19:44:14 +0200

    avoid shadowing vars, use physics process

--- game/Scenes/Projectile/projectile.gd
+++ game/Scenes/Projectile/projectile.gd
@@ -2,19 +2,20 @@ extends Node3D
 class_name Projectile
 
 const PROJECTILE := preload("res://Scenes/Projectile/projectile.tscn")
+
 @export var speed := 50
 
-static func init(position: Vector3, rotation: Vector3) -> Projectile:
+static func init(spawn_position: Vector3, spawn_rotation: Vector3) -> Projectile:
 	var projectile : Projectile = PROJECTILE.instantiate()
-	projectile.position = position
-	projectile.rotation = rotation
+	projectile.position = spawn_position
+	projectile.rotation = spawn_rotation
 	
 	return projectile
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var direction := self.transform.basis.y
 	
-	self.position += direction.normalized() * (speed + 1) * delta
+	self.position += direction.normalized() * speed * delta
 
 func _on_timer_timeout() -> void:
 	self.queue_free()
