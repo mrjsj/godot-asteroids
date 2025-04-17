051d2bc41dd7b8ccfb8b9c9216acde25485906a9
mrjsj <22403319+mrjsj@users.noreply.github.com>
2025-04-17 22:41:12 +0200

    clean up signal connection

--- game/Scripts/projectile_spawner.gd
+++ game/Scripts/projectile_spawner.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 func _ready() -> void:
-    Global.on_gun_fired.connect(Callable(self, "_spawn_projectile"))
+    Global.on_gun_fired.connect(_spawn_projectile)
     
 func _spawn_projectile(emitter: Node3D) -> void:
     var projectile := Projectile.init(
